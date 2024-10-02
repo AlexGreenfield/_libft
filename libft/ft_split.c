6f738fe9523f2a0b654b4ad1ff760f3a66b1cf38
Alejandro Castroverde Rodrigo <acastrov@c1r16s1.42madrid.com>
2024-10-02 16:51:58 +0200

    changes in ft_split.c for readability

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: acastrov <acastrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:46:14 by acastrov          #+#    #+#             */
-/*   Updated: 2024/10/02 16:46:37 by acastrov         ###   ########.fr       */
+/*   Updated: 2024/10/02 16:49:56 by acastrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,51 @@
 
 static size_t	ft_substr_count(char const *s, char c);
 static char		*ft_locate_next_c(char const *s, char c);
-static char		**ft_fill(char const *s, char c, char **split, size_t count);
-static char		**ft_free_split(char **split, size_t count);
+static char		**ft_fill(char const *s, char c, char **split, size_t subcount);
+static char		**ft_free_split(char **split, size_t array_count);
 
 // Splits one string into different substring by an specific char
 char	**ft_split(char const *s, char c)
 {
 	char	**split;
-	size_t	count;
+	size_t	subcount;
 
 	if (!s)
 		return (NULL);
-	count = ft_substr_count(s, c);
-	split = malloc ((count + 1) * sizeof(char *));
+	subcount = ft_substr_count(s, c);
+	split = malloc ((subcount + 1) * sizeof(char *));
 	if (!split)
 		return (NULL);
-	split = ft_fill(s, c, split, count);
+	split = ft_fill(s, c, split, subcount);
 	return (split);
 }
 
 // Static, counts the number of substrings of s separated by char c
 static size_t	ft_substr_count(char const *s, char c)
 {
-	size_t	count;
+	size_t	subcount;
 
-	count = 0;
+	subcount = 0;
 	while (*s)
 	{
 		while (*s == c)
 			s++;
 		if (*s)
-			count++;
+			subcount++;
 		while (*s && *s != c)
 			s++;
 	}
-	return (count);
+	return (subcount);
 }
 
 // Static, fills arrays, calls free if it fails
-static char	**ft_fill(char const *s, char c, char **split, size_t count)
+static char	**ft_fill(char const *s, char c, char **split, size_t subcount)
 {
 	char	*next_c;
 	size_t	array_count;
 
 	array_count = 0;
-	while (*s && array_count < count)
+	while (*s && array_count < subcount)
 	{
 		while (*s == c)
 			s++;
@@ -90,12 +90,12 @@ static char	*ft_locate_next_c(char const *s, char c)
 }
 
 // If error, frees all and return NULL, may need changes
-static char	**ft_free_split(char **split, size_t count)
+static char	**ft_free_split(char **split, size_t array_count)
 {
 	size_t	split_position;
 
 	split_position = 0;
-	while (split_position < count)
+	while (split_position < array_count)
 	{
 		free(split[split_position]);
 		split_position++;
